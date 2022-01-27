3c584aab74ed5e7f7c7a204de6ef516f5ebb85cf
ksthakkar52 <ksthakkar52@gmail.com>
2022-01-26 19:14:28 -0800

    Added new branch, and copied 2020 drivetrain code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public static int DRIVER_PORT = 0;
+    public static int OPERATOR_PORT = 1;
+
+    public static int DRIVE_RIGHT_PORTS[] = {0, 0 ,0};
+    public static int DRIVE_LEFT_PORTS[] = {0, 0, 0};
+
+    public static int NEO_MAX_LIMIT = 45;
+    public static double DRIVE_FORWARD_SPEED = 0.9;
+    public static double DRIVE_REGULAR_SPEED = 0.7;
+    public static double DRIVE_SLOW_SPEED = 0.4;
+    public static double DRIVE_TURBO_SPEED = 0.9;
+    
+}
