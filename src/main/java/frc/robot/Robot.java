996f3e5175bb31681fa1c210b07221ac80b4b540
Karan <ksthakkar52@gmail.com>
2022-02-07 01:53:36 -0800

    Simple abstracted Trajectory creator

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,14 +4,10 @@
 
 package frc.robot;
 
-import java.sql.Driver;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.DriveCommand;
-import frc.robot.subsystems.Drive;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,7 +19,6 @@ public class Robot extends TimedRobot {
 
   public static XboxController driverController;
   public static RobotContainer container;
-
   Command teleopCommand;
   Command autoCommand;
 
---
+++ src/main/java/frc/robot/TrajectoryCreator.java
@@ -0,0 +1,80 @@
+package frc.robot;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
+/**
+ * Abstracted Trajectory generation for for ease of use in
+ * Robot class
+ * 
+ * <p> Need to Add RamseteCommand and OdometryReset<p>
+ */
+public class TrajectoryCreator {
+    public TrajectoryCreator() {
+    }
+
+    /**
+     * Create Trajectories to be run using Ramsete Command.(Use during robot initialization)
+     * 
+     * @param filename       Input a file name Ex: "ExampleTrajectory.wpilib.json"
+     * @param TrajectoryName Input a name for the Trajectory (Can be different than
+     *                       Pathname)
+     * @return A usable pathweaver path in RamseteCommand
+     * @throws IOExecption to DriverStation Console
+     */
+    public Trajectory generateTrajectory(String filename, String TrajectoryName) {
+        Trajectory trajectory = new Trajectory();
+        String trajectoryJSON = "paths/" + filename;
+        try {
+            Path newTrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            trajectory = TrajectoryUtil.fromPathweaverJson(newTrajectoryPath);
+        } catch (IOException ex) {
+            DriverStation.reportError("Unable to open this trajectory: " + TrajectoryName + " (" + trajectoryJSON + ")",
+                    ex.getStackTrace());
+        }
+
+        System.out.println(TrajectoryName + " opened and loaded succesfully.");
+
+        return trajectory;
+
+    }
+    /**
+     * Create Trajectory config
+     * @param startVelocity Input starting velocity in MPS
+     * @param endVelocity Input ending velocity in MPS
+     * @param reversed Reverse Trajectory?
+     * @param kMaxMPS     Input this Constant
+     * @param kMaxAccelMPSsquared   Input this Constant
+     * @param kinematics Input DiffDrive Kinematics Constant
+     * @param voltageConstraint Input Voltage Constraint
+     * @return A trajectoryConfig for manual trajectory generation.
+     */
+
+    public TrajectoryConfig configTrajectory(
+        double startVelocity, double endVelocity, boolean reversed, double kMaxMPS, double kMaxAccelMPSsquared, 
+        DifferentialDriveKinematics kinematics, DifferentialDriveVoltageConstraint voltageConstraint){
+        TrajectoryConfig config = new TrajectoryConfig(
+            kMaxMPS, kMaxAccelMPSsquared)
+            .setKinematics(kinematics)
+            .setReversed(reversed)
+            .setStartVelocity(startVelocity)
+            .setEndVelocity(endVelocity)
+            .addConstraint(voltageConstraint);
+        
+        return config;
+    }
+
+}
