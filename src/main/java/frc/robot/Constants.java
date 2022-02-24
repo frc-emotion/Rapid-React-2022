6c309e8017fec0e81aff6c186835a7c2cf05a7db
Ryan Chaiyakul <ryanchaiyakul@gmail.com>
2022-02-24 09:01:02 -0800

    forgot to change CAN Ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,9 +52,9 @@ public class Constants {
     public static final double SHOOTINDEXINGSPEED = 0.75;
 
     // Shooter Constants
-    public static final int SHOOTER_HOOD_PORT = 0;
-    public static final int SHOOTER_LEFT_PORT = 0;
-    public static final int SHOOTER_RIGHT_PORT = 0;
+    public static final int SHOOTER_HOOD_PORT = 8;
+    public static final int SHOOTER_LEFT_PORT = 9;
+    public static final int SHOOTER_RIGHT_PORT = 10;
     public static final int SHOOTER_LIMIT_PORT = 0;
     public static final double SHOOTER_TARGET_RPM = 5000;
     public static final double SHOOTER_THRESHOLD_RPM = 200;
