9322e3eab19d5bb7a5b5110802ca7f0ce44b52f9
Karan <ksthakkar52@gmail.com>
2022-02-24 00:07:35 -0800

    New interpolator file (WIP)

---
+++ src/main/java/frc/robot/Interpolator.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+/** 
+ * <br> </br>
+ * <i> 
+ * Returns a treeMap of linearly interpolated values between 2 points
+ * </i>
+ * <br> </br>
+ * <b> WIP (Can be optimized a lot)
+ * 
+ */ 
+public class Interpolator{
+    private double x1, y1, x2, y2;
+
+
+    public Interpolator(double x1, double y1, double x2, double y2, double upperBound, double lowerBound){
+        x1 = this.x1;
+        y1 = this.y1;
+        x2 = this.x2;
+        y2 = this.y2;
+    }    
+
+
+    void LinearInterpolate(){
+        //Complete after Sim
+    }
+    
+}
